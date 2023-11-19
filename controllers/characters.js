7f3d97721a123d32ddf69c44a8b5b7877a5cb008
Petal Ladenson <habonimkef@gmail.com>
2023-11-19 13:59:46 -0700

    add database access to index route

--- controllers/characters.js
+++ controllers/characters.js
@@ -5,16 +5,9 @@ const Character = require("../models/character.js");
 // ROUTES (I.N.D.U.C.E.S.)
 
 // INDEX
-router.get("", (req, res) => {
+router.get("", async (req, res) => {
     res.render("character/index.ejs", {
-        characters: [
-            {
-                image: "https://cdn.discordapp.com/attachments/746255447077552179/1173015710595825836/dragon.png",
-                name: "toy",
-                id: 1,
-                quote: "Whoe'er commands a toy obeys\nOut in the stars and far away",
-            },
-        ],
+        characters: await Character.find({}),
     });
 });
 
