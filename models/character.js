278687939c90a46e15123a44f2a9e6fd13fa2d11
Petal Ladenson <habonimkef@gmail.com>
2023-11-19 13:34:11 -0700

    character schema

---
+++ models/character.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const characterSchema = new Schema({
+    name: { type: String, required: true },
+    quote: String,
+    biography: String,
+    age: Number,
+    personality: String,
+    career: String,
+    species: String,
+    magicalAbilities: [String],
+});
+
+const Character = mongoose.model("Character", characterSchema);
+
+module.exports = Character;
