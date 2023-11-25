5165a3766648c13f82e78ca95ebac617827ce6f1
Petal Ladenson <habonimkef@gmail.com>
2023-11-25 15:12:25 -0700

    user can log in

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+const bcrypt = require("bcrypt");
+const express = require("express");
+const users = express.Router();
+const User = require("../models/user.js");
+
+users.get("/new", (req, res) => {
+    res.render("users/new.ejs", {
+        currentUser: req.session.currentUser,
+    });
+});
+
+users.post("/", (req, res) => {
+    //overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(
+        req.body.password,
+        bcrypt.genSaltSync(10)
+    );
+    User.create(req.body, (err, createdUser) => {
+        console.log("user is created", createdUser);
+        res.redirect("/");
+    });
+});
+
+module.exports = users;
--- server.js
+++ server.js
@@ -1,24 +1,46 @@
+//required dependencies
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const characterController = require("./controllers/characters.js");
-
-const mongoURI = process.env.MONGOURI || "mongodb://127.0.0.1:27017/";
 const db = mongoose.connection;
+const methodOverride = require("method-override");
+const session = require("express-session");
 
-app.listen(port, () => console.log("listening on port ", port));
+//controllers
+const characterController = require("./controllers/characters.js");
+const userController = require("./controllers/users.js");
+const sessionsController = require("./controllers/sessions.js");
+
+//env variables
+const mongoURI = process.env.MONGOURI || "mongodb://127.0.0.1:27017/";
+const port = process.env.PORT || 3000;
+
+//MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+app.use(
+    session({
+        secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+        resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+        saveUninitialized: false, // default  more info: https://www.npmjs.com/package/express-session#resave
+    })
+);
+
+//controller middleware
 app.use("/character", characterController);
+app.use("/users", userController);
+app.use("/sessions", sessionsController);
+
+app.listen(port, () => console.log("listening on port ", port));
 mongoose.connect(mongoURI + "char-manager", () => {
     console.log("The connection with mongo is established");
 });
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home.ejs", {
+        user: req.session.currentUser,
+    });
 });
